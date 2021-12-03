ab8bba2f2f80d6e438ec0d0ad07324549e3039af
ytogawa <yusuke.togawa@kuno-corp.com>
2021-12-03 15:41:03 +0900

    validate email

--- src/common/value-objects/email.ts
+++ src/common/value-objects/email.ts
@@ -1,7 +1,13 @@
 import { ValueObject } from '~/common/value-objects/value-object';
+import { DomainException } from '../errors';
 
+const addressRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 export class EmailAddress extends ValueObject<string, 'EmailAddress'> {
   constructor(value: string = '') {
+    if (value !== '' && !addressRegex.test(value)) {
+      throw new DomainException(`value is not email. value=${value}`);
+    }
     super(value);
   }
 }
